136223e2f1012055393ff110777b7bc5cc57b3cc
lai2-24 <lai2-24@itismeucci.com>
2024-05-15 13:34:12 +0200

    consegna

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,41 @@
 import logo from './logo.svg';
+import react, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const[nPartita, setCount]=useState(0);
+  const[numero, setNumero]=useState(null);
+
+
+  async function iniziaPartita(){
+    const avvio = await fetch("http://localhost:8080/partita", {method:"POST"});
+    const data = await avvio.json;
+    setNumero(data.numero);
+    setCount(data.id);
+  }
+
+  async function registraTentativo(){
+    const avvio = await fetch("http://localhost:8080/partita", {method:"POST"});
+    
+  }
+
+
+
   return (
+    <div className='App'>
+      <h3>indovina il numero</h3>
+      <p>numero {numero} </p>
+      <p>contore {nPartita}</p>
+      <button onClick={iniziaPartita}>iniziaPartita</button>
+
+    </div>
+  
+        
+      
+
+    /*
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +51,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
